6405d400a895ee1e47e861abb9825b2733f7aa20
musakhurram <musakhurram248@gmail.com>
2026-08-06 17:23:04 +0500

    validations

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,38 @@
 const express =require('express');
 const router = express.Router();
 const User = require('../models/Users');
+const { body, validationResult } = require('express-validator');
 
+//create a user using: POST "/api/auth" No login required
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
+], async (req, res) => {
 
-//create a user using: POST "/" No login required
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        res.status(201).json({
+            success: true,
+            user
+        });
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            error: err.message
+        });
+    }
 });
 
 module.exports = router;
