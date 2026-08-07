11e10633adcc8ae052b7eafa72119fb5a88e312a
musakhurram <musakhurram248@gmail.com>
2026-08-07 18:08:19 +0500

    initial

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About CloudNote</h1>
+      <p>This is an example note app built with React and React Router.</p>
+    </div>
+  )
+}
+
+export default About
