a03ce1bfb3dde1b1e477589773a9bd16346c25d2
musakhurram <musakhurram248@gmail.com>
2026-08-07 16:26:50 +0500

    update endpoint

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -39,4 +39,31 @@ router.post('/addnote', fetchuser, [
     }
 });
 
+//ROUTE 3: update existing note using: PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be at least 5 characters').isLength({ min: 5 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Note not found");
+        }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: req.body }, { returnDocument: 'after' });
+        res.json(note);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 module.exports = router;
